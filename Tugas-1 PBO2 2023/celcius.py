fb4be3b7bdddf65ae9a7945362e2e954bab3198f
Awaliyah Hayatun <72393657+awaliyahhayatun@users.noreply.github.com>
2023-03-23 13:01:31 +0700

    Add files via upload

---
+++ Tugas-1 PBO2 2023/celcius.py
@@ -0,0 +1,20 @@
+class suhu:
+	def __init__(self, celcius):
+		self._celcius = celcius
+		
+	def get_fahrenhait(self):
+		return (9*self._celcius/5) + 32
+		
+	def get_kelvin(self):
+		return self._celcius + 273
+		
+	def get_reamur(self):
+		return (4/5) * self._celcius
+		
+c = suhu(10)
+
+print (" ----------------- Konversi Celcius ----------------- ")
+print ("")
+print("konversi Celcius ke Fahrenhait : ",c.get_fahrenhait(), "derajat Fahrenhait")
+print("konversi Celcius ke Kelvin : ",c.get_kelvin(), "derajat Kelvin")
+print("konversi Celcius ke Reamur : ",c.get_reamur(), "derajat Reamur")
